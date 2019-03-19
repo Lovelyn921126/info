56db7fd326bac8ef5a3eaa1a067f6b06b72e6588
Lovelyn921126 <a2875372>
2019-03-19 08:57:39 +0800

    config-client2

---
+++ micreoserver-simple-config-client2/src/main/java/com/example/micreoserversimpleprovideruser/MicreoserverSimpleProviderUserApplication.java
@@ -0,0 +1,16 @@
+package com.example.micreoserversimpleprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MicreoserverSimpleProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicreoserverSimpleProviderUserApplication.class, args);
+	}
+	
+
+}
+
